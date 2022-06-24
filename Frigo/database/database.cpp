814154d896b26eb194e8bbec0f53dbe3c0939e65
Gabriel25Oliveira <gabrielosilva@fitec.org.br>
2022-06-24 11:06:23 -0300

    Updates

--- Frigo/database/database.cpp
+++ Frigo/database/database.cpp
@@ -1,6 +1,58 @@
 #include "database.h"
 
-Database::Database()
+bool Database::init()
 {
+    if (database_ != nullptr){
+        return false;
+    }
 
+    database_.reset(new Database());
+    if (database_->open()){
+        //SystemUserQDB::init(database_);
+    }
+
+    return true;
+}
+
+void Database::release()
+{
+    database_.reset();
+}
+
+bool Database::isOpen() const
+{
+    return db_.isOpen();
+}
+
+database_sptr Database::getInstance()
+{
+    return database_;
+}
+
+bool Database::open()
+{
+    if (db_.isOpen()) {
+        return false;
+    }
+
+    db_ = QSqlDatabase::addDatabase("QSQLITE",DatabaseName);
+    db_.setDatabaseName(QString::fromStdString(db_connection));
+    if (!db_.open()) {
+        qDebug() << "Error opening database: "<< db_connection;
+    } else {
+        qDebug()<< "Database OK:"<< db_connection;
+        return true;
+    }
+
+    return false;
+}
+
+bool Database::close()
+{
+    if (db_.isOpen()) {
+        db_.close();
+        return true;
+    }
+
+    return false;
 }
--- Frigo/database/database.h
+++ Frigo/database/database.h
@@ -1,11 +1,39 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <QDebug>
+#include <QtSql/QSql>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlError>
+#include <stdint.h>
+#include <string>
+#include <memory>
+
+class Database;
+typedef std::shared_ptr<Database> database_sptr;
+
+static char *DatabaseName = "FRIGODB";
+static char *db_connection="FRIGODB.db3";
+static char *db_login="";
+static char *db_password="";
+static char *db_schema="";
+static char *db_type="sqlite";
+
+static database_sptr database_(nullptr);
 
 class Database
 {
 public:
-    Database();
+    static bool init();
+    static void release();
+    bool isOpen() const;
+    static database_sptr getInstance();
+    ~Database();
+
+private:
+    bool open();
+    bool close();
+    QSqlDatabase db_;
 };
 
 #endif // DATABASE_H
