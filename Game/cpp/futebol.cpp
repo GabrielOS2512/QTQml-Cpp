2e22ee460e3f4283e2a730b4996dcc2804528682
Gabriel Silva <gabriel.silva@vmimedica.com.br>
2025-05-21 09:01:51 -0300

    Update futebol.cpp

--- Game/cpp/futebol.cpp
+++ Game/cpp/futebol.cpp
@@ -10,7 +10,7 @@ Futebol::Futebol(QObject *parent) : QObject(parent)
 void Futebol::iniciarPartida()
 {
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
-    timeA_ = {"Cruzeiro", 75 , 77, 1.86, 0, 0};
+    timeA_ = {"Cruzeiro", 75 , 77, 1.80, 0, 0};
     timeB_ = {"Vasco", 76 , 72, 1.72, 0, 0};
     //int ovrA = ((timeA_.atk + timeA_.def) / 2) * timeA_.tec;
     //int ovrB = ((timeB_.atk + timeB_.def) / 2) * timeB_.tec;
@@ -45,23 +45,27 @@ int Futebol::calcularChance(Time ataque, Time defesa)
     //Base + influência do ataque e da defesa
     int base_chance = 8;// chance de evento por minuto
     double attack_bonus = (ataque.atk - defesa.def);
-    if (attack_bonus + base_chance > 5)
+    if (attack_bonus + base_chance > 4)
         return attack_bonus + base_chance;
     else
-        return 5;
+        return 4;
 }
 
 void Futebol::chance(Time &time)
 {
-    int n = std::rand() % 100;
-    if(n < 15) {
+    int n = std::rand() % 100; // adiciona verificador de atk x def
+    if(n < 12) {
         qDebug() << "GOL";
         time.gols++;
-    } else if (n < 50) {
-        qDebug() << "DEFESA";
-    } else if (n < 90) {
-        qDebug() << "FORA";
+    } else if (n < 25) {
+        qDebug() << "PERDEU";
+    } else if (n < 45) {
+        qDebug() << "DEFENDEU GOLEIRO";
+    } else if (n < 75) {
+        qDebug() << "DEFESA CORTA";
+    } else if (n < 95) {
+        qDebug() << "CHUTE PRA FORA";
     } else {
-        qDebug() << "TRAVE";
+        qDebug() << "BOLA NA TRAVE";
     }
 }
